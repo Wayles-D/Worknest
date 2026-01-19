817572a29ecccc6dd08529ded45e269202527418
Adefarati22 <adefarati22@gmail.com>
2026-01-19 13:59:42 +0100

    routes updated

---
+++ src/routes/AppRoutes.jsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter, RouterProvider } from "react-router";
+
+
+export default function AppRoutes() {
+    const routes = [
+
+    ]
+    const router = createBrowserRouter(routes)
+  return <RouterProvider router={router} />
+}
