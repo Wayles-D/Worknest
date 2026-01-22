df6b43ac39c1a524531434d36036022d397d16c6
Naya-dev1 <onyekwelibestephanie@gmail.com>
2026-01-22 15:57:16 +0100

    added landing page

--- src/layouts/components/Navbar.jsx
+++ src/layouts/components/Navbar.jsx
@@ -8,22 +8,21 @@ const navLink = [
 
 export default function Navbar() {
   return (
-    <nav className='fixed top-0 left-0 w-full bg-red-500 shadow z-50'>
+    <><nav className='fixed top-0 left-0 w-full bg-red-500 shadow z-50'>
       <div className='flex justify-between items-center p-4'>
-         <img src="/worknestLogo.png" alt="logo" />
+        <img src="/worknestLogo.png" alt="logo" />
 
         <div>
 
         </div>
         <div></div>
 
-         
-      </div>
-    </nav>
-    <>
-    <header className=''>
 
-    </header>
-    </>
+      </div>
+    </nav><>
+        <header className=''>
+
+        </header>
+      </></>
   )
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,24 +1,31 @@
 import React from "react";
+import hero from "../../public/faa45cca433fba8069bed652372bc00b9b9ec941.jpg";
 
 const HomePage = () => {
   return (
-    <div className="px-[]">
-      <div className="grid grid-cols-2 pl-[100px] pr-[99px]">
-        <div>
-          <div>
-            <p className="bg-[#FEEEEA]">OVER 5,O00+ ACTIVE JOBS</p>
-            <h1 className="text-[65px]">Unlock your next career milestone </h1>
+    <div className="mt-30 px-[]">
+      <div className="grid grid-cols-2 items-center justify-between">
+        <div className="w-[549px] ">
+          <div className="flex flex-col items-start gap-[42px]">
+            <p className="bg-[#FEEEEA] text-[14px] py-2.5 px-5 w-[227px] rounded-[15px] text-[#000000] ">
+              OVER 5,O00+ ACTIVE JOBS
+            </p>
+            <h1 className="text-[65px] font-extrabold leading-[70px]  text-[#000000]">
+              Unlock your next{" "}
+              <span className="text-[#F86021]">career milestone</span>{" "}
+            </h1>
             <p>
               Skip the noise. Every listing on WorkNest is hand-picked,
               verified, and posted directly by our platform administrators to
               ensure high-quality career matches.
             </p>
 
-            <div>
-              <div>
+            <div className=" flex border border-[#00000036] rounded-[20px] p-5 gap-2.5">
+              <div >
                 <img src="" alt="" />
                 <p>Search roles or skills...</p>
               </div>
+                <div className="border border-[#000000]"></div>
               <div>
                 <img src="" alt="" />
                 <p>City or remote</p>
@@ -28,8 +35,8 @@ const HomePage = () => {
 
           <button>Search Job</button>
         </div>
-        <div className="bg-cover h-[300px] bg-[url('/faa45cca433fba8069bed652372bc00b9b9ec941.jpg')] bg-center">
-          <input type="text" />
+        <div className="">
+          <img src={hero} alt="" />{" "}
         </div>
       </div>
       {/*  */}
@@ -44,13 +51,12 @@ const HomePage = () => {
             <h6>Filter by role, salary, and tech stack to find your fit.</h6>
           </div>
 
-           <div>
+          <div>
             <p> 2. Apply</p>
             <h6>Filter by role, salary, and tech stack to find your fit.</h6>
           </div>
 
-
-           <div>
+          <div>
             <p> 3. Track</p>
             <h6>Filter by role, salary, and tech stack to find your fit.</h6>
           </div>
