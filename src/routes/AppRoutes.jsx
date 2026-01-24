44c44c399a4588fa6161289d7a75401a9fe58c8a
Adefarati22 <adefarati22@gmail.com>
2026-01-24 16:04:03 +0100

    authFlow

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,6 +1,7 @@
 import { lazy, Suspense } from "react";
 import { createBrowserRouter } from "react-router";
 import SuspenseUi from "@/components/SuspenseUi.jsx";
+import ErrorBoundary from "@/components/ErrorBoundary";
 
 // lazy layouts
 const MainLayout = lazy(() => import("@/layouts/MainLayout.jsx"));
@@ -11,14 +12,12 @@ const AuthLayout = lazy(() => import("@/layouts/AuthLayout.jsx"));
 const Login = lazy(() => import("@/pages/auth/Login.jsx"));
 const Signup = lazy(() => import("@/pages/auth/Signup.jsx"));
 const ForgotPassword = lazy(() => import("@/pages/auth/ForgotPassword.jsx"));
+const ResetPassword = lazy(() => import("@/pages/auth/ResetPassword.jsx"));
+const Verify = lazy(() => import("@/pages/auth/Verify.jsx"));
 const Jobs = lazy(() => import("@/pages/Jobs.jsx"));
 const JobDetails = lazy(() => import("@/pages/JobDetails.jsx"));
-const DashboardHome = lazy(() =>
-  import("@/pages/dashboard/DashboardHome.jsx")
-);
-const DashboardJobs = lazy(() =>
-  import("@/pages/dashboard/DashboardJobs.jsx")
-);
+const DashboardHome = lazy(() => import("@/pages/dashboard/DashboardHome.jsx"));
+const DashboardJobs = lazy(() => import("@/pages/dashboard/DashboardJobs.jsx"));
 
 export const router = createBrowserRouter([
   {
@@ -28,11 +27,15 @@ export const router = createBrowserRouter([
         <AuthLayout />
       </Suspense>
     ),
+    errorElement: <ErrorBoundary />,
+
     children: [
-      { path: "login", element:  <Login /> },
+      { path: "login", element: <Login /> },
       { path: "signup", element: <Signup /> },
-      { path: "forgot-password", element: <ForgotPassword /> }
-    ]
+      { path: "forgot-password", element: <ForgotPassword /> },
+      { path: "reset-password", element: <ResetPassword /> },
+      { path: "verify", element: <Verify /> },
+    ],
   },
   {
     path: "/",
@@ -41,6 +44,8 @@ export const router = createBrowserRouter([
         <MainLayout />
       </Suspense>
     ),
+    errorElement: <ErrorBoundary />,
+
     children: [
       {
         index: true,
@@ -76,6 +81,8 @@ export const router = createBrowserRouter([
         <DashboardLayout />
       </Suspense>
     ),
+    errorElement: <ErrorBoundary />,
+
     children: [
       {
         index: true,
