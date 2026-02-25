b8475ee30668d8761b90aef16f88ce89903c7e0f
Wayles-D <waylesstores@gmail.com>
2026-02-25 12:42:12 +0100

    refactor: remove unused imports and clean up Profile component

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -4,7 +4,7 @@ export default function Logo() {
       const isAdminAuth = location.pathname.startsWith("/admin");
   return (
     <>
-      <NavLink to={isAdminAuth ? "/admin" : "/"}>
+      <NavLink to={isAdminAuth ? "/" : "/"}>
         <img src="/Newlogo.png" alt="logo" className="lg:w-fit h-10 md:w-40" />
       </NavLink>
     </>
